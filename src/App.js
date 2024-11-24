aa4047c547042ff8429c119a54826dd8d0b56e31
MyPortfolio3 <balalanajiib@gmail.com>
2024-11-24 03:02:45 -0600

    Updated routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,24 +4,41 @@ import Portfolio from './components/portfolio';
 import TaskManager from './components/taskmanager';
 
 function App() {
+  // Get the basename from package.json homepage or default to ''
+  const basename = process.env.PUBLIC_URL;
+
   return (
     <div className="App">
       {/* Navigation links */}
       <nav className="bg-gray-800 text-white p-4">
-        <ul className="text-center">
-          <li className="inline-block mx-4">
-            <Link to="/" className="hover:text-gray-400">Portfolio</Link>
-          </li>
-          <li className="inline-block mx-4">
-            <Link to="/task-manager" className="hover:text-gray-400">Task Manager</Link>
-          </li>
-        </ul>
+        <div className="max-w-7xl mx-auto">
+          <ul className="text-center flex justify-center items-center space-x-8">
+            <li>
+              <Link 
+                to={`${basename}/`} 
+                className="hover:text-gray-400 transition-colors duration-200 text-lg font-medium"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link 
+                to={`${basename}/task-manager`} 
+                className="hover:text-gray-400 transition-colors duration-200 text-lg font-medium"
+              >
+                Task Manager
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       {/* Routes for different pages */}
       <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/task-manager" element={<TaskManager />} />
+        <Route path={`${basename}/`} element={<Portfolio />} />
+        <Route path={`${basename}/task-manager`} element={<TaskManager />} />
+        {/* Redirect for the root path */}
+        <Route path="*" element={<Portfolio />} />
       </Routes>
     </div>
   );
